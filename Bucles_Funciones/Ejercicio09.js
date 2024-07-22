8dc306d7c01eea9ef8ce1c05da12d287e4c969ba
Abel <abel.fernandez.martinez@gmail.com>
2024-07-22 18:52:59 +0200

    Update Ejercicio09.js

---
+++ Bucles_Funciones/Ejercicio09.js
@@ -0,0 +1,24 @@
+const capitals = {
+    Spain: "Madrid",
+    France: "Paris",
+    Italy: "Rome",
+    Germany: "Berlin",
+    Portugal: "Lisbon",
+    Poland: "Warsaw",
+    Greece: "Athens",
+    Austria: "Vienna",
+    Hungary: "Budapest",
+    Ireland: "Dublin",
+  };
+  
+  function getCapital(country) {
+    if (capitals.hasOwnProperty(country)) {
+      return capitals[country];
+    } else {
+      return `La capital de ${country} no está en la lista.`;
+    }
+  }
+  
+  console.log(getCapital("Germany"));
+  console.log(getCapital("Austria"));
+  console.log(getCapital("Colombia"));
